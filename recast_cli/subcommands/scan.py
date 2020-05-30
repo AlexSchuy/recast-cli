d4746080b8b60db6b0e52caf988b05c0eca3c77b
vlad <vladov3000@gmail.com>
2020-05-29 23:04:30 -0700

    added scan to command using recast-wf scan.build_multi

--- recast_cli/subcommands/scan.py
+++ recast_cli/subcommands/scan.py
@@ -1,15 +1,25 @@
 import click
+import yaml
+from ..subcommands import *
+
+from ..workflow.recast_workflow.scripts import scan
 
 @click.group(help="RECAST Scan Features")
 def scan():
     pass
 
 @scan.command()
-def build(workflow: str):
+def build(workflow: str, output_path: str):
     """
     Convert existing singlestep-stage workflow into multistage workflow for scans.
 
     :param workflow: singlestep-stage workflow file
     :return:
     """
-    click.echo("building scan...")
+    wf_path = get_workflow_file_path(workflow)
+    with open(wf_path, 'r+') as wf_file:
+        wf_yml = yaml.safe_load(wf_file)
+        wf_yml = scan.build_multi(wf_yml)
+
+    with open(output_path, 'w+') as output_file:
+        yaml.dump(wf_yml, output_file)
